8e98b16c877d3d3c19dfdb7f74330dccc0c2aa4c
student <student@PC111S46.fgakusei.local>
2014-05-26 14:32:40 +0900

    ハロギャラ

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+View.OnClickListener
+{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		//ボタンをIDで探してボタンを変数をつくる
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		 //ボタン変数にリスナーを登録する
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){ //どのボタンが押されたか判定
+		case R.id.btnOK: //btnMsgが押された
+		  //エディットテキストから入力内容を取り出す
+		  EditText etv = (EditText)findViewById(R.id.edtFirst);
+				  String inputMsg = etv.getText().toString();
+		  EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+				  String inputMsg2 = etv2.getText().toString();
+				  
+				  //メッセージ表示用のTextViewを探して、文字を設定
+				  TextView tv = (TextView)findViewById(R.id.txvMsg);
+				  tv.setText("あなたでしたか、" + inputMsg + inputMsg2 + "さん。"); 
+		}
+		
 	}
 
 	@Override
